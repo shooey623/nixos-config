4a6fdadfa60534b1e89484b665ced6680aa8bca7
shooey623 <shooey623@gmail.com>
2026-03-01 18:28:57 +0900

    disable network power saving

--- hosts/zooey/hardware.nix
+++ hosts/zooey/hardware.nix
@@ -42,6 +42,7 @@
   };
 
   networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.powersave = false;
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 }
