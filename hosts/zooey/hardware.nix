dc551f4f58ae83ad6f689abc4b163c95c1e122b0
shooey623 <shooey623@gmail.com>
2026-03-01 18:37:14 +0900

    add virtualization

--- hosts/zooey/hardware.nix
+++ hosts/zooey/hardware.nix
@@ -45,4 +45,17 @@
   networking.networkmanager.wifi.powersave = false;
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
+
+  virtualization.libvirtd = {
+    enable = true;
+    qemu = {
+      swtpm.enable = true;
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
+    };
+  };
+  virtualisatin.spiceUSBRedirection.enable = true;
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = [ "zoey" ];
+  users.groups.kvm.members = [ "zoey" ];
+  environment.systemPackages = with pkgs; [dnsmasq ];
 }
